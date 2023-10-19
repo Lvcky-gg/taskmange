f065f6393b0f28052b72c45567382a2e16820e30
John O'Donnell <matadorcrab@gmail.com>
2023-10-19 15:28:25 -0500

    n/a

--- src/main/java/learn/learn/TaskManager/ui/Controller.java
+++ src/main/java/learn/learn/TaskManager/ui/Controller.java
@@ -1,22 +1,27 @@
 package learn.learn.TaskManager.ui;
 //dont use console here
 
+import learn.learn.TaskManager.data.DataAccessException;
+import learn.learn.TaskManager.domain.TaskResults;
 import learn.learn.TaskManager.domain.TaskService;
+import learn.learn.TaskManager.models.Task;
+
+import java.util.List;
 
 public class Controller {
     private final View view;
-    private final TaskService taskService;
+    private final TaskService service;
 
-    public Controller(View view, TaskService taskService) {
+    public Controller(View view, TaskService service) {
         this.view = view;
-        this.taskService = taskService;
+        this.service = service;
     }
 
     public void run(){
 
     }
 
-    private void runMenu(){
+    private void runMenu() throws DataAccessException {
         boolean exit = false;
         while(!exit){
             int selection = view.getMenuOption();
@@ -41,13 +46,35 @@ public class Controller {
     }
 
 
-    private void addTask(){
+    private void addTask() throws DataAccessException {
+        Task task = view.makeTask();
+        TaskResults res = service.create(task);
+        if(res.isSuccess()){
+            view.displayText("Task was successfully created");
+        }else{
+            view.displayErrors(res.getMessages());
+        }
 
     }
-    private void viewTasks(){
+    private void viewTasks() throws DataAccessException {
+        List<Task> task = service.findALl();
+        view.displayTasks(task);
 
     }
-    private void updateTask(){
+    private void updateTask() throws DataAccessException {
+        view.displayHeader("Update a task.");
+        int id = view.updateById();
+        Task task = service.findById(id);
+        if(task != null){
+            Task updatedTask = view.makeTask();
+            updatedTask.setId(task.getId());
+            TaskResults res = service.update(updatedTask);
+            if(res.isSuccess()){
+                view.displayText("Success!");
+            }
+        }else{
+            view.displayErrors(List.of(String.format("There is no task with id %s", id)));
+        }
 
     }
     private void deleteTask(){
